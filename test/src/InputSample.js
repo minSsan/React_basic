91d04951d471a890b896456bb8ca183f49d2cd63
Park Minseon <msp1125@hanyang.ac.kr>
2021-11-14 11:49:24 +0900

    ~11

--- test/src/InputSample.js
+++ test/src/InputSample.js
@@ -6,11 +6,12 @@ function InputSample() {
         name: '',
         nickname: '',
     });
+    const nameInput = useRef();
 
-    const { name, nickname } = inputs;
+    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
-        const { value, name } = e.target;
+        const { name, value } = e.target;
         console.log(e.target);
         setInputs({
             ...inputs, // 기존의 input 객체를 복사
@@ -23,12 +24,24 @@ function InputSample() {
             name: '',
             nickname: '',
         });
+        nameInput.current.focus();
     };
 
     return (
         <div>
-            <input placeholder="이름" onChange={onChange} value={name}/>
-            <input placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <input 
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}   
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
