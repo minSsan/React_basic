430ebc5af0df6e7def17ab444281eaf06e3d19f2
Park Minseon <msp1125@hanyang.ac.kr>
2021-12-22 23:53:27 +0900

    ~12.22

--- test/src/UserList.js
+++ test/src/UserList.js
@@ -1,7 +1,28 @@
 // 배열 렌더링
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
+    // useEffect(() => {
+    //     console.log('컴포넌트가 화면에 나타남');
+    //     return () => {
+    //         console.log('컴포넌트가 화면에서 사라짐');
+    //     };
+    // }, []);
+
+    useEffect(() => {
+        // 마운트될 때
+        console.log('user 값이 설정됨');
+        console.log(user);
+        // 언마운트될 때
+        return () => {
+            console.log('user 가 바뀌기 전..');
+            console.log(user);
+        };
+    }, [user]);
+    // deps에 값을 넣음 => 컴포넌트가 처음 마운트될 때에도 호출, 지정된 값이
+    // 바뀔 때에도 호출. 그리고 deps 안에 특정 값이 있음 => 언마운트시에도 호출, 
+    // 값이 바뀌기 직전에도 호출
+
     return (
         <div>
             <b 
