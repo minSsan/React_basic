174905d44b20e2a2e257e05d994e3b5cb978994f
Park Minseon <msp1125@hanyang.ac.kr>
2021-12-23 22:40:51 +0900

    -17

--- test/src/App.js
+++ test/src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef, useState, useMemo} from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -13,6 +13,11 @@ import CreateUser from './CreateUser';
 // useRef 객체는 전역 저장소에 저장되기 때문에 함수가 재호출되더라도
 // 마지막으로 업데이트된 current 값이 유지된다. 그리고 매번 동일한 메모리 주소(전역변수)
 // 를 가지기 때문에, 값이 변경되더라도 리렌더링이 되지 않는 것이다. 
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -85,6 +90,17 @@ function App() {
       )
     );
   };
+  
+  // const count = countActiveUsers(users);
+  // 이렇게 호출하게 되면, input 값이 바뀔 때마다 countActiveUsers도 다시 호출됨.
+  // => input의 value 값을 state로 등록했기 때문에,
+  // 해당 값이 바뀔 때마다 전체 화면이 리렌더링되기 때문.
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+  // 첫 번째 파라미터: 어떻게 연산할지 정의하는 함수
+  // 두 번째 파라미터: deps 배열.
+  //    => 넣은 내용이 바뀌면 등록한 함수를 호출해서 값을 연산해줌.
+  //       넣은 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용.
 
   return (
     <>
@@ -99,6 +115,7 @@ function App() {
         onRemove={onRemove}
         onToggle={onToggle}
       />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- test/src/UserList.js
+++ test/src/UserList.js
@@ -3,25 +3,34 @@ import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
     // useEffect(() => {
+    //     // 마운트될 때
     //     console.log('컴포넌트가 화면에 나타남');
-    //     return () => {
+    //     return () => { // cleanup 함수. deps 비어있음 -> 컴포넌트 사라질 때 호출
+    //         // 언마운트될 때
     //         console.log('컴포넌트가 화면에서 사라짐');
     //     };
     // }, []);
+    // deps 배열이 비어있는 경우
+    // -> 컴포넌트 처음 나타날 때만 useEffect에 등록된 함수 호출
 
-    useEffect(() => {
-        // 마운트될 때
-        console.log('user 값이 설정됨');
-        console.log(user);
-        // 언마운트될 때
-        return () => {
-            console.log('user 가 바뀌기 전..');
-            console.log(user);
-        };
-    }, [user]);
+    // useEffect(() => {
+    //     // 마운트될 때
+    //     console.log('user 값이 설정됨');
+    //     console.log(user);
+    //     // 언마운트될 때, user 값이 바뀔 때
+    //     return () => {
+    //         console.log('user 가 바뀌기 전..');
+    //         console.log(user);
+    //     };
+    // }, [user]);
     // deps에 값을 넣음 => 컴포넌트가 처음 마운트될 때에도 호출, 지정된 값이
     // 바뀔 때에도 호출. 그리고 deps 안에 특정 값이 있음 => 언마운트시에도 호출, 
     // 값이 바뀌기 직전에도 호출
+
+    useEffect(() => {
+        console.log(user);
+    });
+    // deps 파라미터를 생략하면 컴포넌트가 리렌더링 될 때마다 호출
 
     return (
         <div>
